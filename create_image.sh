58d40e96f8728ff7c77317ab6d1a2b798ce8dd70
Phil Messenger <phil@Phils-MacBook-Pro.local>
2016-04-26 18:50:54 +0100

    Ensure that cloud-init mounts the first ephemeral device into the right place

--- create_image.sh
+++ create_image.sh
@@ -39,7 +39,7 @@ sudo sed -i 's/.*swap.*//g' /etc/cloud/cloud.cfg
 
 echo "mounts:" >> /etc/cloud/cloud.cfg
 echo " - [ ephemeral0, /mnt, auto, \"defaults,noatime,nodiratime\", "\
-  "\"0\", \"0\" ]" >> /etc/cloud.cloud.cfg
+  "\"0\", \"0\" ]" >> /etc/cloud/cloud.cfg
 
 for x in {1..23}; do
   echo " - [ ephemeral$x, /mnt$((x + 1)), auto, "\
