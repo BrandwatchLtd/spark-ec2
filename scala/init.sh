589b1a1e875382179cc179d9a2eb6ccf9a192418
Phil Messenger <phil@digitalcontortions.net>
2016-09-07 08:11:04 +0100

    upgrade to scala 2.11 and python 3

--- scala/init.sh
+++ scala/init.sh
@@ -7,16 +7,20 @@ if [ -d "scala" ]; then
   return 0
 fi
 
-SCALA_VERSION="2.10.3"
+SCALA_VERSION="2.11.7"
 
 if [[ "0.7.3 0.8.0 0.8.1" =~ $SPARK_VERSION ]]; then
   SCALA_VERSION="2.9.3"
 fi
 
 echo "Unpacking Scala"
-wget http://s3.amazonaws.com/spark-related-packages/scala-$SCALA_VERSION.tgz
+wget https://s3-eu-west-1.amazonaws.com/brandwatch.audiences.bootstrap/scala/scala-$SCALA_VERSION.tgz
 tar xvzf scala-*.tgz > /tmp/spark-ec2_scala.log
 rm scala-*.tgz
 mv `ls -d scala-* | grep -v ec2` scala
 
 popd > /dev/null
+
+echo "Installing Python 3"
+yum -y install python34-devel.x86_64 python34-pip
+pip-3.4 install jupyter boto3 numpy
